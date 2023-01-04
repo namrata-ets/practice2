3129bb3aaa7fff529e2baec645d877b247fda70e
namrata.t <prachi.easternts@gmail.com>
2023-01-04 15:52:34 +0530

    save

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <v-bottom-navigation class="bnav" :value="value" color="primary">
+        <v-btn>
+          <span>Recents</span>
+
+          <v-icon>mdi-history</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Favorites</span>
+
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Nearby</span>
+
+          <v-icon>mdi-map-marker</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+
+      <v-card class="mx-auto" max-width="434" tile>
+        <v-img
+          height="100%"
+          src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+        >
+          <v-row align="end" class="fill-height">
+            <v-col align-self="start" class="pa-0" cols="12">
+              <v-avatar class="profile" color="grey" size="164" tile>
+                <v-img src="./assets/namrata.png"></v-img>
+              </v-avatar>
+            </v-col>
+            <v-col class="py-0">
+              <v-list-item color="rgba(0, 0, 0, .4)" dark>
+                <v-list-item-content>
+                  <v-list-item-title class="text-h6">
+                    Marcus Obrien
+                  </v-list-item-title>
+                  <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-img>
+      </v-card>
+
+      <div class="text-center">
+        <v-pagination v-model="page" :length="7"></v-pagination>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
+<script> </script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.mx-auto {
+  justify-content: center;
+  border-top: 30px;
+  padding-top: 200px;
+  align-items: center;
+}
+.bnav {
+  background-color: tomato;
 }
 </style>
