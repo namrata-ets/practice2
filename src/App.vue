fe653869d1c718123528b27a7f3e91250bcba1a8
namrata.t <prachi.easternts@gmail.com>
2023-01-25 11:11:46 +0530

    initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+var pname=ref('');
+var trips=ref('');
+var airlines=ref('');
+
+const passData=ref(null);
+
+async function fetchData() {
+  // eslint-disable-next-line no-undef
+ 
+ await axios.get(
+    `http://api.instantwebtools.net/v1/passenger?page=0&size=10`, {crossDomain: true}
+  ).then((response)=>passData.value=response.data )
+  .catch(error=>console.log(error))
+  
+}
+
+fetchData()
+
+/*function addPassenger(){
+  pname=pname.value;
+  trips=trips.value;
+  airlines=airlines.value;
+  console.log("Passenger details received are: "+pname.value+trips.value+airlines.value);
+
+}*/
+</script>
+
 <template>
-  <v-app>
-    <v-main>
-      <v-bottom-navigation class="bnav" :value="value" color="primary">
-        <v-btn>
-          <span>Recents</span>
 
-          <v-icon>mdi-history</v-icon>
-        </v-btn>
+  <form @submit.prevent>
+    <h1>First Vue App</h1>
+    <label>Passenger Name </label>
+    <input v-model="pname" placeholder="Name"><br><br>
+    <label>Trips </label>
+    <input v-model="trips" placeholder="No. Of Trips"><br><br>
+    <label>Airlines </label>
+    <input v-model="airlines" placeholder="Airlines"><br><br>
+    <button>Submit</button>
+  </form>
 
-        <v-btn>
-          <span>Favorites</span>
-
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-
-        <v-btn>
-          <span>Nearby</span>
-
-          <v-icon>mdi-map-marker</v-icon>
-        </v-btn>
-      </v-bottom-navigation>
-
-      <v-card class="mx-auto" max-width="434" tile>
-        <v-img
-          height="100%"
-          src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
-        >
-          <v-row align="end" class="fill-height">
-            <v-col align-self="start" class="pa-0" cols="12">
-              <v-avatar class="profile" color="grey" size="164" tile>
-                <v-img src="./assets/namrata.png"></v-img>
-              </v-avatar>
-            </v-col>
-            <v-col class="py-0">
-              <v-list-item color="rgba(0, 0, 0, .4)" dark>
-                <v-list-item-content>
-                  <v-list-item-title class="text-h6">
-                    Marcus Obrien
-                  </v-list-item-title>
-                  <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-col>
-          </v-row>
-        </v-img>
-      </v-card>
-
-      <div class="text-center">
-        <v-pagination v-model="page" :length="7"></v-pagination>
-      </div>
-    </v-main>
-  </v-app>
 </template>
 
-<script> </script>
 <style>
-.mx-auto {
-  justify-content: center;
-  border-top: 30px;
-  padding-top: 200px;
-  align-items: center;
-}
-.bnav {
-  background-color: tomato;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import axios from 'axios';
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .mount('#app')
+
+  axios.defaults.headers.common = {
+    "X-Requested-With": "XMLHttpRequest",
+    "X-CSRFToken": "example-of-custom-header",
+    "Access-Control-Allow-Origin" :"*"
+  };
+
+  axios.defaults.headers.post['xsrfCookieName'] = 'CSRFToken';
+  axios.defaults.headers.post['xsrfHeaderName'] = 'X-CSRFToken';
+  axios.defaults.headers.post['responseType'] = 'json';
+  axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
